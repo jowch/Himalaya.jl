c11f8f45b6d927094d518d47f89ea2ef8a05a28a
Jonathan Chen <jwhc@ucla.edu>
2022-05-12 15:41:32 -0700

    refactor `indexpeaks` to be more general

--- src/Himalaya.jl
+++ src/Himalaya.jl
@@ -3,17 +3,22 @@ module Himalaya
 using Peaks
 using Statistics
 
-import Base: ==
+import Base: ==, issubset
 
 export 
 
+# phases
+Phase, Lamellar, Hexagonal, Pn3m, Im3m, Ia3d, Fd3m, phaseratios,
+
 # indexing
-Index, indexpeaks, predictpeaks, score, fit, npeak, ==,
+Index, phase, basis, peaks, numpeaks, predictpeaks, missingpeaks, ==, issubset,
+indexpeaks, remove_subsets, fit, score,
 
 # peaks
 findpeaks
 
 # include("util.jl")
+include("phase.jl")
 include("peakfinding.jl")
 include("index.jl")
 
