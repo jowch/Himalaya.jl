e9c910edcfb7eb55704aeea739199602930ca89e
Jonathan Chen <jwhc@ucla.edu>
2022-11-09 23:31:26 -0800

    add Square lattice phase

--- src/Himalaya.jl
+++ src/Himalaya.jl
@@ -10,7 +10,7 @@ import Base: ==, issubset, show
 export 
 
 # phases
-Phase, Lamellar, Hexagonal, Pn3m, Im3m, Ia3d, Fd3m, phaseratios,
+Phase, Lamellar, Hexagonal, Square, Pn3m, Im3m, Ia3d, Fd3m, phaseratios,
 
 # indexing
 Index, phase, basis, peaks, numpeaks, totalprom, predictpeaks, missingpeaks,
--- src/phase.jl
+++ src/phase.jl
@@ -37,6 +37,7 @@ abstract type Phase end
 abstract type Cubic <: Phase end
 abstract type Lamellar <: Phase end
 abstract type Hexagonal <: Phase end
+abstract type Square <: Phase end
 abstract type Pn3m <: Cubic end
 abstract type Im3m <: Cubic end
 abstract type Ia3d <: Cubic end
@@ -58,9 +59,11 @@ function phaseratios(::Type{P}; normalize = false) where {P<:Phase}
 end
 
 phaseratios(::Type{Lamellar}) = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-phaseratios(::Type{Hexagonal}) = [1, √3, √4, √7, √9, √11, √12]
+phaseratios(::Type{Hexagonal}) = [1, √3, √4, √7, √9, √11, √12, √13, √16, √19,
+                                  √21, √25, √27, √28]
+phaseratios(::Type{Square}) = [1, √2, √4, √5, √8, √9, √10, √13, √16, √17, √18, √20]
 phaseratios(::Type{Pn3m}) = [√2, √3, √4, √6, √8, √9, √10, √11, √12, √14, √16,
-                             √17, √18, √19, √20, √21] #, √22, √24, √26]
+                             √17, √18, √19, √20, √21] #, √22, √24, √26, √27]
 phaseratios(::Type{Im3m}) = [√2, √4, √6, √8, √10, √12, √14, √16, √18, √20] #, √22, √24, √26]
 phaseratios(::Type{Ia3d}) = [√6, √8, √14, √16, √20, √22, √24, √26]
 phaseratios(::Type{Fd3m}) = [√3, √8, √11, √12, √16, √19, √24, √27, √32, √35, √36]
@@ -78,6 +81,7 @@ end
 
 minpeaks(::Type{Lamellar}) = 2
 minpeaks(::Type{Hexagonal}) = 3
+minpeaks(::Type{Square}) = 3
 minpeaks(::Type{Pn3m}) = 4
 minpeaks(::Type{Im3m}) = 4
 minpeaks(::Type{Ia3d}) = 4
