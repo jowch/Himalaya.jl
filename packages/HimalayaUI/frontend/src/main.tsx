835c34396d6a63a0d4ffd5ecb6c873753f7ae32b
Jonathan Chen <jwhc@ucla.edu>
2026-04-29 14:41:16 -0700

    chore(ui): install boneyard-js skeleton framework

--- packages/HimalayaUI/frontend/src/main.tsx
+++ packages/HimalayaUI/frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import "./bones/registry";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
